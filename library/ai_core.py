005d14e54c4bea7278f5826be199512f436c8807
Arvan Tan <vtanixdev666@gmail.com>
2026-02-22 22:04:06 +0700

    >X(

--- library/ai_core.py
+++ library/ai_core.py
@@ -1,23 +1,27 @@
-from library.tts_engine import make_wav, send_audio
+from library.tts_engine import speak_stream
 from library.esp32_gateway import send_cmd
 
 def ask_ai(text):
+    # nanti ganti ke LLM
     return """SPEECH: Lamp turned on.
 CMD:LIGHT=ON"""
 
 def handle_text(text):
     reply = ask_ai(text)
+
     speech = ""
     cmds = []
 
     for line in reply.splitlines():
         if line.startswith("SPEECH:"):
             speech = line[7:].strip()
-        if line.startswith("CMD:"):
+        elif line.startswith("CMD:"):
             cmds.append(line[4:].strip())
 
-    wav_file = make_wav(speech)
-    send_audio(wav_file)
-
+    # send cmd to ESP32 via BT/Serial first
     for c in cmds:
         send_cmd(c)
+
+    # TTS STREAM to ESP32 for speech response
+    if speech:
+        speak_stream(speech)
