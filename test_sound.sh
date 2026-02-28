2855fe86a9b886c3fba58f0a5db6a98eaf65aa62
Arvan Tan <vtanixdev666@gmail.com>
2026-02-28 22:41:40 +0700

    Update test_sound.sh

--- test_sound.sh
+++ test_sound.sh
@@ -1,6 +1,22 @@
-echo "testing sound.."
-
+echo "================================"
+echo "          Test Sound"
+echo "================================"
 echo ""
-nmap -sn 192.168.1.0/24
+cek() {
+    read -p "$1 (y/n): " jaw
+    case "$jaw" in
+        [yY][eE][sS]|[yY])
+        nmap -sn 192.168.1.0/24
+        ;;
+    *)
+        return 0
+        ;;
+    esac
+}
 
-python ~/Android-Esp32/library/test_sound.py
+if cek "scan ip addreas?"; then
+    echo "scanning.."
+    echo ""
+else
+    python ~/Android-Esp32/library/test_sound.py
+fi
